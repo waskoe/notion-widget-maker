230f90336acc528a525977d42bf07d30be8281e0
waskoe <98802604+waskoe@users.noreply.github.com>
2023-08-11 00:16:06 -0400

    Update script.js

--- streak-counter/script.js
+++ streak-counter/script.js
@@ -2,12 +2,15 @@
 var checkbox = document.getElementById('checkbox');
 var streakCountElement = document.getElementById('streak-count');
 var streakElement = document.getElementById('streak');
-var streakCount = 11;
+var daysLeftElement = document.getElementById('days-left');
+var streakCount = 10;
 
 function updateStreak() {
     if (checkbox.checked) {
+        checkbox.disabled = true; // Disable checkbox once it's checked
         streakCount++;
     } else {
+        checkbox.disabled = false; // Re-enable checkbox if unchecked
         streakCount = 0;
     }
 
@@ -19,7 +22,19 @@ function updateStreak() {
     var dayOfWeek = today.getDay();
     if (!checkbox.checked && (dayOfWeek === 0 || dayOfWeek === 1)) {
         streakElement.classList.add('red');
+        showDaysLeft(today);
+    } else {
+        hideDaysLeft();
     }
+}
+
+function showDaysLeft(today) {
+    var daysLeft = 7 - today.getDay();
+    daysLeftElement.textContent = 'Days left: ' + daysLeft;
+}
+
+function hideDaysLeft() {
+    daysLeftElement.textContent = '';
 }
 
 checkbox.addEventListener('change', updateStreak);
