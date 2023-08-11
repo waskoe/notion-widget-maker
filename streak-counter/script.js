91a37862c5cec51eb1daff6d5ff70c5fa6df8f3b
waskoe <98802604+waskoe@users.noreply.github.com>
2023-08-11 01:02:15 -0400

    Update script.js

--- streak-counter/script.js
+++ streak-counter/script.js
@@ -1,28 +1,44 @@
-// script.js
-let lastChecked = false;
-let streak = 10;
-let lastStreak = streak;
+const checkbox = document.getElementById('checkbox');
+const streak = document.getElementById('streak');
+const fire = document.getElementById('fire');
+const daysLeft = document.getElementById('days-left');
 
-function updateStreak() {
-    const checkbox = document.getElementById("checkbox");
-    const streakCount = document.getElementById("streak-count");
-    const daysLeft = document.getElementById("days-left");
+// Load streak count from local storage
+let currentStreak = parseInt(localStorage.getItem('streak')) || 10;
+streak.textContent = currentStreak;
 
-    if (lastChecked !== checkbox.checked) {
-        lastChecked = checkbox.checked;
+// Set initial checkbox state
+checkbox.checked = localStorage.getItem('checkbox') === 'true';
 
-        if (checkbox.checked) {
-            const currentDate = new Date();
-            if (currentDate.getDay() === 0 && streak !== lastStreak) {
-                streak++;
-                lastStreak = streak;
-            }
-            daysLeft.textContent = "";
-        } else {
-            daysLeft.textContent = `Days left: ${7 - new Date().getDay()} days`;
+// Update streak count and checkbox state
+checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+        currentStreak++;
+    } else {
+        currentStreak--;
+        if (currentStreak < 0) {
+            currentStreak = 0;
         }
+    }
+    streak.textContent = currentStreak;
+    localStorage.setItem('streak', currentStreak);
+    localStorage.setItem('checkbox', checkbox.checked);
+    updateDaysLeft();
+});
 
-        streakCount.textContent = streak;
-        streakCount.className = (streak <= 2 && !checkbox.checked) ? "red" : "";
+// Display days left in the current week
+function updateDaysLeft() {
+    if (!checkbox.checked) {
+        const daysUntilSunday = 7 - new Date().getDay();
+        if (daysUntilSunday > 2) {
+            daysLeft.textContent = `Days left: ${daysUntilSunday} days`;
+        } else {
+            daysLeft.textContent = `Calls for a date this week`;
+        }
+    } else {
+        daysLeft.textContent = '';
     }
 }
+
+updateDaysLeft();
+setInterval(updateDaysLeft, 1000 * 60 * 60); // Update every hour
