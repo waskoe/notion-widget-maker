ce9d4923dc35964eceac4cb0ab6758fd7d62eb27
waskoe <98802604+waskoe@users.noreply.github.com>
2023-08-11 00:36:03 -0400

    Update script.js

--- streak-counter/script.js
+++ streak-counter/script.js
@@ -1,47 +1,23 @@
 // script.js
-var checkbox = document.getElementById('checkbox');
-var streakCountElement = document.getElementById('streak-count');
-var streakElement = document.getElementById('streak');
-var daysLeftElement = document.getElementById('days-left');
-var streakCount = 10;
+let lastChecked = false;
+let streak = 10;
 
-// Function to update the streak and related elements
 function updateStreak() {
-    if (checkbox.checked) {
-        checkbox.disabled = true; // Disable checkbox once it's checked
-        streakCount++;
-    } else {
-        checkbox.disabled = false; // Re-enable checkbox if unchecked
-        streakCount = 0;
-    }
+    const checkbox = document.getElementById("checkbox");
+    const streakCount = document.getElementById("streak-count");
+    const daysLeft = document.getElementById("days-left");
 
-    streakCountElement.textContent = streakCount;
-    streakElement.classList.remove('red');
+    if (lastChecked !== checkbox.checked) {
+        lastChecked = checkbox.checked;
 
-    // Add the red class if streak is inactive for the last two days
-    var today = new Date();
-    var dayOfWeek = today.getDay();
-    if (!checkbox.checked && (dayOfWeek === 0 || dayOfWeek === 1)) {
-        streakElement.classList.add('red');
-        showDaysLeft(today);
-    } else {
-        hideDaysLeft();
+        if (checkbox.checked) {
+            streak++;
+            daysLeft.textContent = "";
+        } else {
+            daysLeft.textContent = `Days left: ${7 - new Date().getDay()} days`;
+        }
+
+        streakCount.textContent = streak;
+        streakCount.className = (streak <= 2 && !checkbox.checked) ? "red" : "";
     }
 }
-
-// Function to show days left until end of the week
-function showDaysLeft(today) {
-    var daysLeft = 7 - today.getDay();
-    daysLeftElement.textContent = 'Days left: ' + daysLeft;
-}
-
-// Function to hide days left
-function hideDaysLeft() {
-    daysLeftElement.textContent = '';
-}
-
-// Add event listener to checkbox for updates
-checkbox.addEventListener('change', updateStreak);
-
-// Load initial streak state on page load
-updateStreak();
