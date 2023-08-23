3e9a3f77eabd6c39054fbc1689cd0cf3b05e40b9
waskoe <98802604+waskoe@users.noreply.github.com>
2023-08-23 13:06:27 -0400

    Update script.js

--- streak-counter/script.js
+++ streak-counter/script.js
@@ -1,29 +1,40 @@
-const checkbox = document.getElementById('streakCheckbox');
-const streakNumber = document.getElementById('streakNumber');
-const streakMessage = document.getElementById('streakMessage');
+const checkbox = document.getElementById('checkbox');
+const numberElement = document.getElementById('number');
+const warningElement = document.getElementById('warning');
 
-checkbox.addEventListener('change', updateStreak);
+let streakNumber = 12; // Starting point
 
-function updateStreak() {
-    if (checkbox.checked) {
-        streakNumber.textContent = parseInt(streakNumber.textContent) + 1;
-        streakMessage.style.display = 'none';
-    } else {
-        streakNumber.textContent = parseInt(streakNumber.textContent) - 1;
-        streakMessage.style.display = 'none';
-    }
+function updateStreakNumber() {
+  numberElement.textContent = streakNumber;
+
+  if (streakNumber === 0) {
+    warningElement.style.display = 'none';
+  } else if (streakNumber > 0 && streakNumber <= 2) {
+    warningElement.style.display = 'block';
+  }
 }
 
-// Simulate automatic unticking after a week (7 seconds for demonstration)
-setTimeout(() => {
-    checkbox.checked = false;
-    updateStreak();
-    streakMessage.style.display = 'block';
-}, 7000); // Change this to 7 days (7 * 24 * 60 * 60 * 1000) for a week
-
-// Reset streak on click and remove the message
-streakMessage.addEventListener('click', () => {
-    streakNumber.textContent = 0;
-    checkbox.checked = true;
-    streakMessage.style.display = 'none';
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    streakNumber++;
+    updateStreakNumber();
+  } else {
+    streakNumber = Math.max(0, streakNumber - 1);
+    updateStreakNumber();
+  }
 });
+
+function checkForNewWeek() {
+  const now = new Date();
+  const day = now.getDay(); // 0 (Sunday) to 6 (Saturday)
+  const time = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+
+  if (day === 0 && time >= 23 * 3600 + 59 * 60) {
+    // Reset the streak number if one calendar week has passed
+    streakNumber = 0;
+    updateStreakNumber();
+  }
+}
+
+updateStreakNumber();
+setInterval(checkForNewWeek, 60000); // Check every minute
