7ba74fd54ca36ee7792bfba30303fc1b2f211981
waskoe <98802604+waskoe@users.noreply.github.com>
2023-08-23 13:16:19 -0400

    Update script.js

--- streak-counter/script.js
+++ streak-counter/script.js
@@ -1,40 +1,41 @@
 const checkbox = document.getElementById('checkbox');
-const numberElement = document.getElementById('number');
-const warningElement = document.getElementById('warning');
+const streakNumber = document.getElementById('streakNumber');
+const warningText = document.getElementById('warningText');
 
-let streakNumber = 12; // Starting point
+let currentStreak = 12; // Starting point
+let lastCheckedTime = new Date();
 
-function updateStreakNumber() {
-  numberElement.textContent = streakNumber;
-
-  if (streakNumber === 0) {
-    warningElement.style.display = 'none';
-  } else if (streakNumber > 0 && streakNumber <= 2) {
-    warningElement.style.display = 'block';
-  }
-}
-
-checkbox.addEventListener('change', () => {
-  if (checkbox.checked) {
-    streakNumber++;
-    updateStreakNumber();
+checkbox.addEventListener('change', function () {
+  if (this.checked) {
+    currentStreak += 1;
+    lastCheckedTime = new Date();
+    warningText.style.display = 'none';
   } else {
-    streakNumber = Math.max(0, streakNumber - 1);
-    updateStreakNumber();
+    currentStreak -= 1;
   }
+  updateStreak();
 });
 
-function checkForNewWeek() {
-  const now = new Date();
-  const day = now.getDay(); // 0 (Sunday) to 6 (Saturday)
-  const time = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+function updateStreak() {
+  streakNumber.textContent = currentStreak;
+}
 
-  if (day === 0 && time >= 23 * 3600 + 59 * 60) {
-    // Reset the streak number if one calendar week has passed
-    streakNumber = 0;
-    updateStreakNumber();
+function checkStreak() {
+  const currentTime = new Date();
+  const daysPassed = Math.floor((currentTime - lastCheckedTime) / (1000 * 60 * 60 * 24));
+  
+  if (daysPassed >= 7) {
+    currentStreak = 0;
+    lastCheckedTime = currentTime;
+    updateStreak();
+  }
+
+  if (daysPassed >= 5 && daysPassed < 7) {
+    warningText.style.display = 'block';
+  } else {
+    warningText.style.display = 'none';
   }
 }
 
-updateStreakNumber();
-setInterval(checkForNewWeek, 60000); // Check every minute
+checkStreak();
+setInterval(checkStreak, 1000 * 60 * 60); // Check every hour
